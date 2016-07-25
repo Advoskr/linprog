0482b15a2d2e39ab404d5143f54f04924a1e2e7b
Alex Voskresenskiy <advoskr@lement.pro>
2016-07-25 22:35:23 +0300

    Слегка переделал структуру классов чтения таски, слил Task и Simplex_Task

--- MsmSolver/MsmSolver/BaseClasses/Task.cs
+++ MsmSolver/MsmSolver/BaseClasses/Task.cs
@@ -9,5 +9,23 @@ namespace MsmSolver
         public Matrix A { get; set; }
         public Vector C { get; set; }
         public Vector A0 { get; set; }
+
+        public Signs[] Signs { get; set; }
+
+        public Direction Direction { get; set; }
+
+        public Task()
+        {
+
+        }
+
+        public Task(Vector c, Matrix a, Vector a0, Signs[] s, Direction dir)
+        {
+            this.C = c;
+            this.A = a;
+            this.A0 = a0;
+            this.Signs = s;
+            this.Direction = dir;
+        }
     }
 }
