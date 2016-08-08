541302722c738fdf0fa639c8ee2ca06a9a48298c
Евгений Карулин < >
2016-08-08 19:51:25 +0300

    Читалка Жолобовских задач

--- MsmSolver/TestConsoleForEverything/Program.cs
+++ MsmSolver/TestConsoleForEverything/Program.cs
@@ -18,72 +18,82 @@ namespace TestConsoleForEverything
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine(Math.Sign(-0.0014f));
-            
-            //RunMultipleIterations();
-            //const int cols = 300;
-            //const int rows = 300;
-            //const int doubleSize = sizeof(double);
-            double[][] rectArray = new double[rows][];
-            for (int i = 0; i < rows; i++)
-            {
-                rectArray[i] = new double[cols];
-                for (int j = 0; j < cols; j++)
-                {
-                    rectArray[i][j] = i + j + 1;
-                }
-            }
+            /*   //Console.WriteLine(Math.Sign(-0.0014f));
 
-            var matrix = new MsmSolver.Matrix();
-            matrix.Initialize(rectArray);
-            var sw = Stopwatch.StartNew();
-            var mp = new StrassenMathOperationsProvider();
-            MsmSolver.Matrix result = null;
-            int counter=10;
-            for (int i = 0; i < counter; i++)
-            {
-                result = mp.Multiply(matrix, matrix);
-            }
-            Console.WriteLine("Columns:"+result.ColCount);
-            sw.Stop();
-            Console.WriteLine(sw.Elapsed.TotalMilliseconds);
+               //RunMultipleIterations();
+               const int cols = 300;
+               const int rows = 300;
+               //const int doubleSize = sizeof(double);
+               double[][] rectArray = new double[rows][];
+               for (int i = 0; i < rows; i++)
+               {
+                   rectArray[i] = new double[cols];
+                   for (int j = 0; j < cols; j++)
+                   {
+                       rectArray[i][j] = i + j + 1;
+                   }
+               }
 
-            var matrix2 = new Matrix(cols, rows);
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < cols; j++)
-                {
-                    matrix2[i,j] = i + j+1;
-                }
-            }
-            
-            sw.Restart();
-            Matrix res = null;
-            for (int i = 0; i < counter; i++)
-            {
-                res = matrix2*matrix2;
-            }
-            Console.WriteLine("Columns:" + res.cols);
-            sw.Stop();
-            Console.WriteLine(sw.Elapsed.TotalMilliseconds);
+               var matrix = new MsmSolver.Matrix();
+               matrix.Initialize(rectArray);
+               var sw = Stopwatch.StartNew();
+               var mp = new StrassenMathOperationsProvider();
+               MsmSolver.Matrix result = null;
+               int counter=10;
+               for (int i = 0; i < counter; i++)
+               {
+                   result = mp.Multiply(matrix, matrix);
+               }
+               Console.WriteLine("Columns:"+result.ColCount);
+               sw.Stop();
+               Console.WriteLine(sw.Elapsed.TotalMilliseconds);
 
-            for (int i = 0; i < result.RowCount; i++)
-            {
-                for (int j = 0; j < result.ColCount; j++)
-                {
-                    if(res[i,j]!=result[i,j])
-                        throw new Exception("!");
-                }
-            }
+               var matrix2 = new Matrix(cols, rows);
+               for (int i = 0; i < rows; i++)
+               {
+                   for (int j = 0; j < cols; j++)
+                   {
+                       matrix2[i,j] = i + j+1;
+                   }
+               }
 
-            //int counter=10000;
-            //for (int i = 0; i < counter; i++)
-            //    {
-            //{
-            //    var cl = new SomeClass(rectArray);
-            //sw.Stop();
-            //Console.WriteLine(sw.Elapsed.TotalMilliseconds);
-            Console.ReadLine();
+               sw.Restart();
+               Matrix res = null;
+               for (int i = 0; i < counter; i++)
+               {
+                   res = matrix2*matrix2;
+               }
+               Console.WriteLine("Columns:" + res.cols);
+               sw.Stop();
+               Console.WriteLine(sw.Elapsed.TotalMilliseconds);
+
+               for (int i = 0; i < result.RowCount; i++)
+               {
+                   for (int j = 0; j < result.ColCount; j++)
+                   {
+                       if(res[i,j]!=result[i,j])
+                           throw new Exception("!");
+                   }
+               }
+
+               //int counter=10000;
+               //for (int i = 0; i < counter; i++)
+               //    {
+               //{
+               //    var cl = new SomeClass(rectArray);
+               //sw.Stop();
+               //Console.WriteLine(sw.Elapsed.TotalMilliseconds);
+               Console.ReadLine();*/
+
+              RunMultipleIterations();
+              Console.ReadLine();
+
+         //   var z = new Task();
+           // StreamReader reader = new StreamReader("Zadacha 300x300.txt");
+          //  z = new GTaskReader().ReadFromGFile(reader);
+
+           // Console.ReadLine();
+
         }
 
         private class SomeClass
@@ -102,7 +112,7 @@ namespace TestConsoleForEverything
         private static void RunMultipleIterations()
         {
             List<double> Jenia = new List<double>(5);
-            int execCount = 4;
+            int execCount = 5;
             Answer answer = null;
             var sw = Stopwatch.StartNew();
             for (int i = 0; i < execCount; i++)
@@ -110,7 +120,7 @@ namespace TestConsoleForEverything
                 answer = ModularSolverCaller("Zadacha 300x300.txt");
                 Jenia.Add(sw.Elapsed.TotalMilliseconds);
                 sw.Stop();
-                Console.WriteLine($"Iteration: {i} completed");
+               Console.WriteLine($"Iteration: {i} completed");
                 sw.Restart();
             }
 
@@ -147,9 +157,9 @@ namespace TestConsoleForEverything
             Task z;
             using (var reader = new StreamReader(taskFile))
             {
-                z = new TaskReader().ReadFromSmallFile(reader);
+                z = new GTaskReader().ReadFromGFile(reader);
             }
-            var mathProvider = new StrassenMathOperationsProvider();
+            var mathProvider = new CudaMathOperationsProvider();
             
             var answer = new ModularSolver(mathProvider, new FullParallelDeltasCalculator(mathProvider), new StraightVectorToBasisPutter(mathProvider),
                 new FirstIncomingVectorFinder(), new MTaskBasisFinder(), new SimpleOutgoingVectorFinder())
